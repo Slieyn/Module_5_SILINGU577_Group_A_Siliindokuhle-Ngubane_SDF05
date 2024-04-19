14c8a6500840e949c5032690e3467ff10c4eaaa2
Silindokuhle Ngubane <silindokuhlengubane0@gmail.com>
2024-04-19 20:15:59 +0200

    // use of "oncclick for functionality"// //used <buttons>buttons</>for functionality // I have removed the emoji's for readibility// //use of arrays,objects,booleans,if else statements// //use of logical operations; for loops; The Math Object and Return statements//

---
+++ cofd94b318377a92708266d5b/startGame.js
@@ -0,0 +1,13 @@
+function startGame() {
+    if (sum <= 20) {
+        message = "Do you want to draw a new card? 🙂";
+    } else if (sum === 21) {
+        message = "Wohoo! You've got Blackjack! 🥳";
+        hasBlackJack = true;
+    } else {
+        message = "You're out of the game! 😭";
+        isAlive = false;
+    }
+
+    console.log(message);
+}
